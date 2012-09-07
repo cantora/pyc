73c789ad18cdf9f21f629fd563dbeff9c8dcd0a3
cantora <cantora@yob.(none)>
2012-09-06 23:35:51 -0600

    stack size bug fix

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 FILE=$(dirname $1)/$(basename $1 .py )
-./pyc ${FILE}.py && cat ${FILE}.s && { for i in *.c; do gcc -m32 -c $i; done; } && gcc -m32 -o output ${FILE}.s runtime.o hashtable.o hashtable_utility.o hashtable_itr.o -lm && ./output 
+./pyc ${FILE}.py && cat ${FILE}.s && { for i in *.c; do gcc -m32 -c $i; done; } && gcc -m32 -o output ${FILE}.s runtime.o hashtable.o hashtable_utility.o hashtable_itr.o -lm 
+
