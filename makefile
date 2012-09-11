b3d4febf67fd6fd0ad17ea755f45f23117508910
cantora <cantora@yob.(none)>
2012-09-10 23:42:47 -0600

    makefile for clib

--- makefile
+++ makefile
@@ -16,6 +16,9 @@ hw.zip:
 
 .PHONY: clean
 clean:
+	rm -fv hw.zip
 	rm -fv output
+	rm -fv ./clib/*.o
 	rm -fv *.pyc
 	for i in $$(find ./test/ -regex '.*\.\(\(expected\)\|\(out\)\|\(s\)\)'); do rm -v $$i; done
+
--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+set -e
+
 FILE=$(dirname $1)/$(basename $1 .py )
-./pyc ${FILE}.py && cat ${FILE}.s && { for i in *.c; do gcc -m32 -c $i; done; } && gcc -m32 -o output ${FILE}.s runtime.o hashtable.o hashtable_utility.o hashtable_itr.o -lm 
+
+./pyc ${FILE}.py 
+cat ${FILE}.s 
+for i in ./clib/*.c; do 
+  bash -c "cd $(dirname $i) && gcc -m32 -c $i"
+done
+
+#runtime.o hashtable.o hashtable_utility.o hashtable_itr.o
+gcc -m32 -o output ${FILE}.s ./clib/*.o -lm 
 
