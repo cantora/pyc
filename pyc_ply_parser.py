34cb2f74205b3a1c6f771f3e82dd58d0fcf251ad
cantora <cantora@yob.(none)>
2012-09-11 18:41:57 -0600

    assign expr, call expr, ident expr

--- pyc_ply_parser.py
+++ pyc_ply_parser.py
@@ -1,14 +1,33 @@
+'''
+identifier ::=  (letter|"_") (letter | digit | "_")*
+letter     ::=  lowercase | uppercase
+lowercase  ::=  "a"..."z"
+uppercase  ::=  "A"..."Z"
+digit      ::=  "0"..."9"
+'''
+
+reserved = {
+	'print': 'PRINT'
+}
+
+literals = ['=', '(', ')']
+
 tokens = (
-	'PRINT', 
 	'INT', 
 	'PLUS', 
 	'MINUS',
-	'SEMI'
-)
+	'SEMI',
+	'IDENT'
+) + tuple(reserved.values())
 
-t_PRINT = r'print'
 t_PLUS = r'\+'
 t_MINUS = r'-'
+t_SEMI = r';'
+
+def t_IDENT(t):
+	r'[a-zA-Z_][a-zA-Z0-9_]*'
+	t.type = reserved.get(t.value, 'IDENT')
+	return t
 
 def t_INT(t):
 	r'\d+'
@@ -26,9 +45,6 @@ def t_newline(t):
 	r'\n+'
 	t.lexer.lineno += t.value.count("\n")
 
-def t_SEMI(t):
-	r';+'
-
 def t_error(t):
 	print "illegal character '%s'" % t.value[0]
 
@@ -37,7 +53,7 @@ import pyc_util
 pyc_util.add_relative_dir_to_syspath("ply")
 import ply.lex
 
-ply.lex.lex()
+ply.lex.lex(debug=True)
 
 import compiler
 precedence = (
@@ -69,20 +85,40 @@ def p_statement(t):
 
 	t[0] = t[1]
 
-def p_print_statement(t):
-	'stmt : PRINT expression'
+def p_print_stmt(t):
+	'stmt : PRINT expr'
 	t[0] = compiler.ast.Printnl([t[2]], None)
 
-def p_plus_expression(t):
-	'expression : expression PLUS expression'
+
+def p_assign_stmt(t):
+	'stmt : IDENT "=" expr'
+
+	t[0] = compiler.ast.Assign(
+		[compiler.ast.AssName(t[1], 'OP_ASSIGN')],
+		t[3]
+		)
+
+def p_call_expr(t):
+	'expr : IDENT "(" ")"'
+	
+	t[0] = compiler.ast.CallFunc(compiler.ast.Name(t[1]), [])
+		
+
+def p_ident_expr(t):
+	'expr : IDENT'
+	
+	t[0] = compiler.ast.Name(t[1])
+
+def p_plus_expr(t):
+	'expr : expr PLUS expr'
 	t[0] = compiler.ast.Add( (t[1], t[3]) )
 
-def p_neg_expression(t):
-	'expression : MINUS expression %prec UMINUS'
+def p_neg_expr(t):
+	'expr : MINUS expr %prec UMINUS'
 	t[0] = compiler.ast.UnarySub(t[2])
 
-def p_int_expression(t):
-	'expression : INT'
+def p_int_expr(t):
+	'expr : INT'
 	t[0] = compiler.ast.Const(t[1])
 
 def p_error(t):
@@ -90,7 +126,16 @@ def p_error(t):
 
 import ply.yacc
 
-ply.yacc.yacc()
+ply.yacc.yacc(debug=True)
+
+import logging
 
 def parse(src):
-	return ply.yacc.parse(src)
+	log = logging.getLogger()
+	
+	# create console handler and set level to debug
+	ch = logging.StreamHandler()
+	ch.setLevel(logging.DEBUG)
+	log.addHandler(ch)
+
+	return ply.yacc.parse(src, debug=log)
