0724dbf1c39104f63eaee61b22b92d816e54be64
cantora <cantora@yob.(none)>
2012-09-20 17:34:55 -0600

    reverted parser to using python compiler package

--- pyc_parser.py
+++ pyc_parser.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-#import compiler
-#def parse(src):
-	#return compiler.parse(src)
-
-import pyc_ply_parser
-
+import compiler
 def parse(src):
-	return pyc_ply_parser.parse(src)
+	return compiler.parse(src)
+
+#import pyc_ply_parser
+
+#def parse(src):
+#	return pyc_ply_parser.parse(src)
