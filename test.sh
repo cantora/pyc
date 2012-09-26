84befd31339d5264046372583639a4bf7b95105b
anthony cantor <tony.cantor@gmail.com>
2012-09-26 00:35:26 -0600

    added VERBOSE setting to test.sh

--- test.sh
+++ test.sh
@@ -4,26 +4,35 @@ set -e
 FILE=$(dirname $1)/$(basename $1 .py )
 
 ./pyc ${FILE}.py 
-cat ${FILE}.s 
-make -C ./clib
+if [ ! "$VERBOSE" = "0" ]; then
+  cat ${FILE}.s 
+fi
+
+make -s -C ./clib
 
 gcc -m32 -o output ${FILE}.s ./clib/*.o -lm
 ./output < ${FILE}.in > ${FILE}.out 
 python ${FILE}.py < ${FILE}.in > ${FILE}.expected || true
 
 set +e
-echo "output:"
-cat ${FILE}.out
-echo "#####################"
-echo "expected:"
-cat ${FILE}.expected
-echo "#####################"
-echo "out vs expected"
-diff ${FILE}.out ${FILE}.expected
+if [ ! "$VERBOSE" = "0" ]; then
+  echo "output:"
+  cat ${FILE}.out
+  echo "#####################"
+  echo "expected:"
+  cat ${FILE}.expected
+  echo "#####################"
+  echo "out vs expected"
+fi
 
-if [ $? == 0 ]; then
-  echo "test passed"
+if [ ! "$VERBOSE" = "0" ]; then
+  diff ${FILE}.out ${FILE}.expected
+  if [ $? == 0 ]; then
+    echo "test passed"
+  else
+    echo "test failed"
+  fi
 else
-  echo "test failed"
+  diff ${FILE}.out ${FILE}.expected
 fi
 
