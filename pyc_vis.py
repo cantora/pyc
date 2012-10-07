8886008bfa1b26bfcde4b505fde650ee637b633f
anthony cantor <tony.cantor@gmail.com>
2012-10-07 15:25:46 -0600

    added logging to pyc_vis.py

--- pyc_vis.py
+++ pyc_vis.py
@@ -2,6 +2,7 @@ class Visitor:
 
 	def __init__(self):
 		self.depth = 0
+		self.log = None
 
 	def default(self, node, *args):
 		raise Exception('no visit method for type %s in %s at depth %d' \
@@ -12,6 +13,9 @@ def dispatch(instance, node, *args):
 	klass = node.__class__
 	className = klass.__name__
 	meth = getattr(instance, 'visit_' + className, instance.default)
+
+	if hasattr(instance.log, '__call__'):
+		instance.log("%s%s => %s" % (" "*instance.depth, node.__class__.__name__, meth.__name__) )
 
 	return meth(node, *args)
 
