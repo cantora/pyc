13c0080254e9d6f0c3eaa5e59b526a463b613e03
anthony cantor <tony.cantor@gmail.com>
2012-10-07 20:15:27 -0600

    improved gen_name to work with arbitrary prefixes

--- pyc_gen_name.py
+++ pyc_gen_name.py
@@ -1,8 +1,18 @@
 
-gen_count = 0
-def new():
-	global gen_count
-	gen_count += 1
-	n = "gen_%d" % gen_count
+prefix_count = {
+	"gen_": 0
+}
+
+def new(prefix="gen_"):
+	global prefix_count
+	if not prefix in prefix_count:
+		prefix_count[prefix] = 0
+
+	prefix_count[prefix] += 1
+	n = "%s%d" % (prefix, prefix_count[prefix])
 	
 	return n
+
+def user_name(s):
+	return "user_%s" % s
+
